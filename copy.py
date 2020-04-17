8987947788b9535906ff9e7a7487590f18e9af58
BR@5t0N <31829702+13A5T0N@users.noreply.github.com>
2020-04-17 17:38:43 -0300

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,36 @@
+import shutil,glob,os,datetime
+from pathlib import Path
+
+print("place wich folder you want to make a backup")
+src_dir = input()
+print("to wich folder do you want to place the backup")
+dst_dir = input()
+
+curent_date = datetime.datetime.now()
+file_name = str(curent_date.strftime("%d-%m-%Y"))+ ".txt"
+
+#make a folder
+folder_name = str(curent_date.strftime("%d-%m-%Y")) + "_"+ str(curent_date.strftime("%H:%M"))
+new_folder = dst_dir+folder_name
+path = Path(str(new_folder))
+    
+if path.is_dir():
+    print("file already exist")
+else:
+    folder = os.mkdir(new_folder)
+
+os.chdir(src_dir)
+total = 0 
+
+for file in os.listdir(src_dir):
+    # type file you want a copy of 
+    if file.endswith(".xlsx"):
+        file = os.path.join(src_dir, file)
+        shutil.copy(file,new_folder)
+        total += 1
+
+file = open(file_name,"w+")
+file.write("Amount of Files \r\n")
+file.write(str(total))
+file.close
+shutil.move(file_name,new_folder)
